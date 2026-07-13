e0e63e0baf9bdf1d85d24c87a687b974937463a6
sidhesh-gadhari <sidheshgadhari16@gmail.com>
2026-07-13 21:26:51 +0530

    Configure repo for Heroku deployment

--- Backend/Server.js
+++ Backend/Server.js
@@ -11,7 +11,7 @@ require('./Config/OAuth20.config.js');
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true
 }));
 app.use(express.json({limit: "16kb"}));
